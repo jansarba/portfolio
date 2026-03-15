40857f1356323bd63aad23d190c6df90480a814d
jansarba <jansarbasarba@gmail.com>
2026-03-15 20:19:46 +0100

    react-pdf

---
+++ src/config/pdf.ts
@@ -0,0 +1,8 @@
+import { pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
+
+export const PDF_FILE = '/Portfolio_JanSarba_Fullsize.pdf';
